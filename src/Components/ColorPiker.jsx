82139a107a295d19f69594522ee4c533f9490246
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-07-26 18:24:20 +0530

    catch

---
+++ src/Components/ColorPiker.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import { SketchPicker } from 'react-color';
+
+function ColorPiker() {
+  return (
+    <div>
+      <SketchPicker />
+    </div>
+  )
+}
+
+export default ColorPiker
